231f9f62ade3267f846f02c872b2122cc57e6ba5
Erik <glebellier@protonmail.com>
2021-10-17 00:40:45 +0200

    Minor changes

--- src/weatherpy/algo.py
+++ src/weatherpy/algo.py
@@ -1,12 +1,14 @@
 import os
 from pprint import pprint
 import pandas as pd
+import numpy as np
 import seaborn as sns
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from weatherpy.infos import SPOTIPY_REDIRECT_URL, SPOTIPY_CLIENT_SECRET, SPOTIPY_CLIENT_ID
 from weatherpy.playlist import Playlist
+from weatherpy.subset_sum_algo import get_subset_idx
 
 os.environ["SPOTIPY_CLIENT_ID"] = SPOTIPY_CLIENT_ID
 os.environ["SPOTIPY_CLIENT_SECRET"] = SPOTIPY_CLIENT_SECRET
@@ -18,10 +20,16 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 source = Playlist(sp, "63emxHLY69jlsRJ18UXCU8")
 playlist = Playlist(sp, "066ZKIpH5hDMmDd5HnyuWr")
 
-source.get_metadata()
-pprint(source.metadata[["name", "valence", "loudness"]])
-pprint(source.get_means())
+# df = source.get_metadata().select_dtypes(include=['int64', 'float64'])
+# print(df)
+# df = df.apply(lambda x: (x - x.min()) / (x.max() - x.min()), axis=0)
+# print(df)
+# # we get the mean of the whished features
+# m_features = source.get_means()[["valence", "loudness"]]
+# print(m_features.to_numpy())
 
-playlist.get_metadata()
-pprint(playlist.metadata[["name", "valence", "loudness"]])
-pprint(playlist.get_means())
+S = np.random.random((20, 10))
+T = np.random.random((1, 10))
+
+print("_____________")
+print(get_subset_idx(S, T, 3))
