62a1e00362f12f289514bc4c655066fa86e2d8cb
Erik <glebellier@protonmail.com>
2021-10-18 00:16:32 +0200

    Add playlist updating according to the target

--- src/weatherpy/main.py
+++ src/weatherpy/main.py
@@ -14,7 +14,7 @@ from weatherpy.algo import get_weather_playlist
 os.environ["SPOTIPY_CLIENT_ID"] = SPOTIPY_CLIENT_ID
 os.environ["SPOTIPY_CLIENT_SECRET"] = SPOTIPY_CLIENT_SECRET
 os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:8000"
-scope = "user-library-read"
+scope = "user-library-read playlist-modify-public"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
@@ -25,7 +25,17 @@ destination_uri = "066ZKIpH5hDMmDd5HnyuWr"
 features = ["tempo", "acousticness", "liveness"]
 target = np.random.random((1, len(features)))
 
-weather_pl = get_weather_playlist(sp, source_uri, destination_uri, target,
-                                  features)
-print(weather_pl["uri"])
-print(weather_pl.columns.values)
+# TODO : make sure the playlist length is > given length(min)
+weather_pl = get_weather_playlist(sp,
+                                  source_uri,
+                                  destination_uri,
+                                  target,
+                                  features,
+                                  nb_tracks=10)
+
+print(weather_pl["name"])
+# we need to extract the tracks ids
+track_ids = [elt.split(":")[-1] for elt in list(weather_pl["uri"])]
+
+results = sp.playlist_replace_items(destination_uri, track_ids)
+pprint(results)
