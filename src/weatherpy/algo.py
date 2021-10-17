bb22b1389f953f2997fdedb457ba8703165f84cc
Erik <glebellier@protonmail.com>
2021-10-17 13:09:53 +0200

    Refactor code and and tests

--- src/weatherpy/algo.py
+++ src/weatherpy/algo.py
@@ -2,44 +2,32 @@ import os
 from pprint import pprint
 import pandas as pd
 import numpy as np
-import seaborn as sns
-
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from weatherpy.infos import SPOTIPY_REDIRECT_URL, SPOTIPY_CLIENT_SECRET, SPOTIPY_CLIENT_ID
 from weatherpy.playlist import Playlist
 from weatherpy.subset_sum_algo import get_subset_idx
 
-os.environ["SPOTIPY_CLIENT_ID"] = SPOTIPY_CLIENT_ID
-os.environ["SPOTIPY_CLIENT_SECRET"] = SPOTIPY_CLIENT_SECRET
-os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:8000"
-scope = "user-library-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+def get_weather_playlist(API, source_uri, destination_uri, target, features):
 
-source = Playlist(sp, "63emxHLY69jlsRJ18UXCU8")
-destination = Playlist(sp, "066ZKIpH5hDMmDd5HnyuWr")
+    source = Playlist(API, source_uri)
+    destination = Playlist(API, destination_uri)
 
-# collect tracks audio features (numerical ones)
-s_data = source.get_metadata()
-d_data = destination.get_metadata()
+    # collect tracks audio features (numerical ones)
+    s_data = source.get_metadata()
+    d_data = destination.get_metadata()
 
-s = s_data.select_dtypes(include=['int64', 'float64'])
-d = d_data.select_dtypes(include=['int64', 'float64'])
+    # check if all features in the playlist audio features
+    assert all([feature in s_data.columns.values for feature in features
+                ]), "Features not all in Playlist audio features"
 
-# we normalize the source and the destination playlist audio features
-norm = lambda x: (x - x.min()) / (x.max() - x.min())
-s = s.apply(norm, axis=0)
-d = d.apply(norm, axis=0)
+    s = s_data.select_dtypes(include=['int64', 'float64'])
+    d = d_data.select_dtypes(include=['int64', 'float64'])
 
-print(s)
-# defining the features we want to take into account
-features = ["tempo", "acousticness", "liveness"]
+    # we normalize the source and the destination playlist audio features
+    norm = lambda x: (x - x.min()) / (x.max() - x.min())
+    s = s.apply(norm, axis=0)
+    d = d.apply(norm, axis=0)
 
-S = s[features]
-target = np.random.random((1, len(features)))
-
-print("_____________")
-print(target)
-indexes = get_subset_idx(S, target, 3).squeeze()
-print(s_data.iloc[list(indexes)])
+    # we only take into account the given features
+    s = s[features]
+    indexes = get_subset_idx(s, target, 3).squeeze()
+    return s_data.iloc[list(indexes)]
--- src/weatherpy/main.py
+++ src/weatherpy/main.py
@@ -1,73 +1,31 @@
+import os
+from pprint import pprint
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 from weatherpy.infos import SPOTIPY_REDIRECT_URL, SPOTIPY_CLIENT_SECRET, SPOTIPY_CLIENT_ID
-import requests
-import base64
-import datetime
-from urllib.parse import urlencode
+from weatherpy.playlist import Playlist
+from weatherpy.subset_sum_algo import get_subset_idx
+from weatherpy.algo import get_weather_playlist
 
+os.environ["SPOTIPY_CLIENT_ID"] = SPOTIPY_CLIENT_ID
+os.environ["SPOTIPY_CLIENT_SECRET"] = SPOTIPY_CLIENT_SECRET
+os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:8000"
+scope = "user-library-read"
 
-class SpotifyAPI(object):
-    URL = 'https://accounts.spotify.com/api/token'
-    METHOD = "POST"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-    def __init__(self, client_id, secret_key):
-        super().__init__()
-        self.access_token = None
-        self.access_token_expires = datetime.datetime.now()
-        self.access_token_expired = True
-        self.client_id = None
-        self.secret_key = None
-        self.client_cred = base64.b64encode(
-            (f"{client_id}:{secret_key}").encode())
+source_uri = "63emxHLY69jlsRJ18UXCU8"
+destination_uri = "066ZKIpH5hDMmDd5HnyuWr"
 
-    def get_token_headers(self):
-        return {"Authorization": f"Basic {self.client_cred.decode()}"}
+# defining the features we want to take into account
+features = ["tempo", "acousticness", "liveness"]
+target = np.random.random((1, len(features)))
 
-    def get_token_data(self):
-        return {"grant_type": "client_credentials"}
-
-    def get_auth(self):
-
-        r = requests.post(self.URL,
-                          data=self.get_token_data(),
-                          headers=self.get_token_headers())
-        valid_response = r.status_code in range(200, 299)
-        if not valid_response: raise Exception("Authentication failed")
-        else:
-            data = r.json()
-
-            self.access_token = data["access_token"]
-            expires_in = data["expires_in"]
-            self.access_token_expires = datetime.datetime.now(
-            ) + datetime.timedelta(seconds=expires_in)
-
-            self.access_token_expired = self.access_token_expires < datetime.datetime.now(
-            )
-            return True
-
-    def get_access_token(self):
-        token = self.access_token
-        if self.access_token_expires < datetime.datetime.now():
-            self.get_auth()
-            return self.get_access_token()
-        elif token == None:
-            self.get_auth()
-            return self.get_access_token()
-        return token
-
-    def search(self, query, search_type="artist"):
-        self.get_access_token()
-        headers = {"Authorization": f"Bearer {self.access_token}"}
-        endpoint = "https://api.spotify.com/v1/search"
-
-        data = urlencode({"q": query, "type": search_type.lower()})
-        lookup_url = f"{endpoint}?{data}"
-
-        r = requests.get(lookup_url, headers=headers)
-        if not r.status_code in range(200, 299):
-            return {}
-        return r.json()
-
-
-API = SpotifyAPI(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
-print(API.search("Time", search_type="Track"))
-print(API.access_token)
+weather_pl = get_weather_playlist(sp, source_uri, destination_uri, target,
+                                  features)
+print(weather_pl["uri"])
+print(weather_pl.columns.values)
