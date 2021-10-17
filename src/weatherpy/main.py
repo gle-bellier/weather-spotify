681146a4d0116a696c80e86d48a5b95a2f06ebc6
Erik <glebellier@protonmail.com>
2021-10-18 01:02:17 +0200

    Use weather API results as target for the playlist

--- src/weatherpy/main.py
+++ src/weatherpy/main.py
@@ -6,7 +6,10 @@ import seaborn as sns
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from weatherpy.infos import SPOTIPY_REDIRECT_URL, SPOTIPY_CLIENT_SECRET, SPOTIPY_CLIENT_ID
+from weatherpy.infos import SPOTIPY_REDIRECT_URL, SPOTIPY_CLIENT_SECRET, SPOTIPY_CLIENT_ID, WEATHER_API_KEY
+
+from weatherpy.weather import Weather
+
 from weatherpy.playlist import Playlist
 from weatherpy.subset_sum_algo import get_subset_idx
 from weatherpy.algo import get_weather_playlist
@@ -16,13 +19,18 @@ os.environ["SPOTIPY_CLIENT_SECRET"] = SPOTIPY_CLIENT_SECRET
 os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:8000"
 scope = "user-library-read playlist-modify-public"
 
+w = Weather(WEATHER_API_KEY)
+weather = w.get_weather("Paris")
+weather_features = list(weather.values())
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 source_uri = "63emxHLY69jlsRJ18UXCU8"
 destination_uri = "066ZKIpH5hDMmDd5HnyuWr"
 
+print("Target : ", weather_features)
 # defining the features we want to take into account
-features = ["tempo", "acousticness", "liveness"]
+features = ["tempo", "acousticness", "liveness", "energy"]
 target = np.random.random((1, len(features)))
 
 # TODO : make sure the playlist length is > given length(min)
@@ -33,8 +41,10 @@ weather_pl = get_weather_playlist(sp,
                                   features,
                                   nb_tracks=10)
 
-print(weather_pl["name"])
 # we need to extract the tracks ids
+
+# TODO : normalize audio features in the playlist
+print(weather_pl[features])
 track_ids = [elt.split(":")[-1] for elt in list(weather_pl["uri"])]
 
 results = sp.playlist_replace_items(destination_uri, track_ids)
