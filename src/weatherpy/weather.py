d53a492315c21450ff5d98afd119b4a6538e5907
Erik <glebellier@protonmail.com>
2021-10-17 15:30:09 +0200

    Add weather data collecting and normalization

--- src/weatherpy/weather.py
+++ src/weatherpy/weather.py
@@ -1,6 +1,7 @@
 import requests
 from weatherpy.infos import WEATHER_API_KEY
 import datetime
+import numpy as np
 
 
 class Weather:
@@ -16,12 +17,35 @@ class Weather:
 
         if weather_expired or self.weather is None:
             self.weather = requests.get(
-                f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&APPID={self.WEATHER_API_KEY}'
+                f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&APPID={self.WEATHER_API_KEY}&units=metric'
             ).json()
 
     def get_weather(self, CITY):
         self.update_weather(CITY)
-        return self.weather
+        # we collect the data we want
+        wind = self.weather["wind"]["speed"]
+        cloudiness = self.weather["clouds"]["all"]
+        temperature = self.weather["main"]["temp"]
+        humidity = self.weather["main"]["humidity"]
+
+        # we need to normalize them
+        # TODO : work with more data to provide better normalization
+
+        # cloudiness and humidity "are" %
+        cloudiness /= 100
+        humidity /= 100
+
+        # we decide that the t° expressive range is [0, 35]°C
+        temperature = np.round(np.clip(temperature / 35, 0, 1), decimals=3)
+        # we set wind speed range to [0, 15] m/s
+        wind = np.round(np.clip(wind / 15, 0, 1), decimals=3)
+
+        return {
+            "temperature": temperature,
+            "humidity": humidity,
+            "cloudiness": cloudiness,
+            "wind": wind
+        }
 
 
 w = Weather(WEATHER_API_KEY)
